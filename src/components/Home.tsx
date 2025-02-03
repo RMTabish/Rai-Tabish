27a20f2551cf82af9c4f7aadf6cd85f222966311
rmtabi123 <tabish@knodemy.com>
2025-02-03 21:17:03 +0500

    cv update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -89,8 +89,8 @@ export default function Home() {
           </div>
         </Link>
         <a
-  href="/Rai Tabish.pdf"
-  download="Rai Tabish.pdf"
+  href="/Rai Tabish FAST-NUCES-ISB.pdf"
+  download="Rai Tabish FAST-NUCES.pdf"
   className="inline-flex items-center px-4 py-2 mt-4 border border-gray-600 text-gray-600 rounded-full hover:bg-gray-600 hover:text-white transition-colors duration-200 cursor-pointer group"
 >
   <span className="mr-2">Download Resume</span>
